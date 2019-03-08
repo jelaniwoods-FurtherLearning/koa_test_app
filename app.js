ce7329278e4ea20556035de876e53ec66efe77b1
Jelani Woods <jelani@firstdraft.com>
2019-03-08 14:06:04 -0600

    Add router

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
 
 
 const app = new Koa();
+const router = new KoaRouter();
+
+// JSON Pretty Middleware
 app.use(json);
 // easy api service
 // app.use(async ctx => ctx.body = {msg: "Hello World"});
 // There's an extra module koa-json
-app.use(async ctx => ctx.body = "Hello World");
+// app.use(async ctx => ctx.body = "Hello World");
+
+router.get("/test", ctx => ctx = "Hello World");
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
+
 
 app.listen(3000, () => console.log("Server Started..."));
