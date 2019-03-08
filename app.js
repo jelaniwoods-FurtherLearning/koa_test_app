28bf1d812ad9c984ac741a6aa4e324cd16b30995
Jelani Woods <jelani@firstdraft.com>
2019-03-08 14:07:51 -0600

    Fix typo in json

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = new Koa();
 const router = new KoaRouter();
 
 // JSON Pretty Middleware
-app.use(json);
+app.use(json());
 // easy api service
 // app.use(async ctx => ctx.body = {msg: "Hello World"});
 // There's an extra module koa-json
 // app.use(async ctx => ctx.body = "Hello World");
 
-router.get("/test", ctx => ctx = "Hello World");
+router.get("/test", ctx => (ctx.body = "Hello World"));
 
 // Router Middleware
 app.use(router.routes()).use(router.allowedMethods());
