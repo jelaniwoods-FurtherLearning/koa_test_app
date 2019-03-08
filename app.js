1db5cc6ce29e6d2f3ff9e65aef3686fd4a446ce9
Jelani Woods <jelani@firstdraft.com>
2019-03-08 14:04:21 -0600

    wip

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const Koa = require('koa');
+const json = require('koa-json');
+
 
 const app = new Koa();
-
+app.use(json);
+// easy api service
+// app.use(async ctx => ctx.body = {msg: "Hello World"});
+// There's an extra module koa-json
 app.use(async ctx => ctx.body = "Hello World");
 
 app.listen(3000, () => console.log("Server Started..."));
